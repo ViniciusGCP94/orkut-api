46c1899e1beaabe734eda93402123ed9ef6a0522
ViniciusGCP94 <vinniegcp@gmail.com>
2026-03-25 11:29:23 -0300

    feat: integrar postgresql e implementar rotas de posts (get/post)
    - Configuração do Pool de conexão com node-pg
    - Implementação de listagem de posts com JOIN (usuarios/posts)
    - Criação de endpoint para inserção de novas postagens
    - Estruturação de variáveis de ambiente com dotenv

--- src/app.js
+++ src/app.js
@@ -5,27 +5,62 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("<h1>Rede Social!</h1>");
+    res.send("<h1>Rede Social!</h1>")
 });
 
-// GET DOS POSTS
-
-app.get("/post", async (req, res) => {
-  try {
-    const resultado = await pool.query(`
-        SELECT
-            usuarios.nome,
-            post.conteudo,
-            post.criado_em
-        FROM post
-        JOIN usuarios
-        ON post.usuario_id = usuarios.id
-        ORDER BY post.criado_em DESC
+app.get("/usuarios", async (req, res) => {
+    try {
+        const resultado = await pool.query(`
+            SELECT * FROM usuarios;
         `);
-    res.json(resultado.rows);
+        res.json(resultado.rows)
+    } catch (erro) {
+        res.status(500).json({erro: "Erro ao buscar dados de usuários"});
+    }
+});
+
+app.get("/posts", async (req, res) => {
+    try {
+        const resultado = await pool.query(`
+            SELECT
+                usuarios.id,
+                usuarios.nome,
+                post.titulo,
+                post.conteudo,
+                post.criado_em
+            FROM post
+            JOIN usuarios
+            ON post.usuario_id = usuarios.id
+            ORDER BY post.criado_em DESC
+        `);
+        res.json(resultado.rows);
+
+    } catch (erro) {
+        res.status(500).json({erro: "Erro ao buscar postagens"})
+    }
+})
+
+app.post("/post", async (req, res) => {
+  try {
+    const { titulo, conteudo, usuario_id } = req.body;
+    const resultado = await pool.query(
+      `
+      INSERT INTO post (titulo, conteudo, usuario_id)
+      VALUES ($1, $2, $3)
+      RETURNING *
+      `,
+      [titulo, conteudo, usuario_id],
+    );
+    res.status(201).json({
+      mensagem: "Post criado com sucesso",
+      post: resultado.rows[0],
+    });
   } catch (erro) {
-    res.status(500).json({ erro: "Erro ao buscar postagens" });
+    res.status(500).json({
+      erro: "Erro ao criar postagem",
+    });
   }
 });
+
 
 module.exports = app;
